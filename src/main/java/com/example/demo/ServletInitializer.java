2afc1e4f7ae68249fb45c8d9448ec8f94dae1dc7
veena malleypally <54092758+veensdamu16@users.noreply.github.com>
2024-02-23 20:45:47 +0530

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import org.springframework;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
