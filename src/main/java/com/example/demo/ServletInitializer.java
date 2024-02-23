9527438ea01f822466b7424c4bde3fe3ce94b866
veena malleypally <54092758+veensdamu16@users.noreply.github.com>
2024-02-23 20:02:15 +0530

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -3,8 +3,12 @@ package com.example.demo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import org.springframework
 
-public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
